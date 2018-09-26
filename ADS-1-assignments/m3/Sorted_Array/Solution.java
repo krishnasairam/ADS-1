c09b1ba4e990f5e0437a981d1506961f780cdf92
krishnasairam <sairamadabala007@msitprogram.net>
2018-09-26 20:34:02 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- ADS-1-assignments/m3/Sorted_Array/Solution.java
+++ ADS-1-assignments/m3/Sorted_Array/Solution.java
@@ -1,81 +1,81 @@
 import java.util.Scanner;
 import java.util.Arrays;
 /**
- * Sorted_Array.
+ * Sortedarray
  */
-class Sorted_Array {
-	/**
-	 * pairs array.
-	 */
-	private int[] pairs;
-	/**
-	 * size of array.
-	 */
-	private int size;
-	/**
-	* Constructs the object.
-	*/
-	public Sorted_Array(final int n) {
-		size = 0;
-		pairs = new int[n];
-	}
-	/**
-	 * add.
-	 *
-	 * @param      x     { parameter_description }
-	 */
-	public void add(final int x) {
-		pairs[size++] = x;
-	}
-	/**
-	 * sort.
-	 */
-	public void sort() {
-		Arrays.sort(pairs);
-		String s = "";
-		for (int i : pairs) {
-			s += i + ",";
-		}
-		s = s.substring(0, s.length() - 1);
-		System.out.println(s);
-	}
+class Sortedarray{
+    /**
+     * pairs array.
+     */
+    private int[] pairs;
+    /**
+     * size of array.
+     */
+    private int size;
+    /**
+    * Constructs the object.
+    */
+    public Sortedarray(final int n) {
+        size = 0;
+        pairs = new int[n];
+    }
+    /**
+     * add.
+     *
+     * @param      x     { parameter_description }
+     */
+    public void add(final int x) {
+        pairs[size++] = x;
+    }
+    /**
+     * sort.
+     */
+    public void sort() {
+        Arrays.sort(pairs);
+        String s = "";
+        for (int i : pairs) {
+            s += i + ",";
+        }
+        s = s.substring(0, s.length() - 1);
+        System.out.println(s);
+    }
 }
 /**
  * solution class.
  */
 public final class Solution {
-	/**
-	* Constructs the object.
-	*/
-	private Solution() {
-		// leave this blank
-	}
-	/**
-	 * main function to execute test cases.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(
-		    System.in);
-		int n = scan.nextInt();
-		int m = scan.nextInt();
-		String[] list1;
-		String[] list2;
-		Sorted_Array p = new Sorted_Array(m + n);
-		scan.nextLine();
-		if (n != 0) {
-			list1 = scan.nextLine().split(",");
-			for (String i : list1) {
-				p.add(Integer.parseInt(i));
-			}
-		} else {
-			scan.nextLine();
-		}
-		list2 = scan.nextLine().split(",");
-		for (String j : list2) {
-			p.add(Integer.parseInt(j));
-		}
-		p.sort();
-	}
+    /**
+    * Constructs the object.
+    */
+    private Solution() {
+        // leave this blank
+    }
+    /**
+     * main function to execute test cases.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(
+            System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        String[] list1;
+        String[] list2;
+        Sortedarray p = new Sortedarray(m + n);
+        scan.nextLine();
+        if (n != 0) {
+            list1 = scan.nextLine().split(",");
+            for (String i : list1) {
+                p.add(Integer.parseInt(i));
+            }
+        } else {
+            scan.nextLine();
+        }
+        list2 = scan.nextLine().split(",");
+        for (String j : list2) {
+            p.add(Integer.parseInt(j));
+        }
+        p.sort();
+    }
 }
