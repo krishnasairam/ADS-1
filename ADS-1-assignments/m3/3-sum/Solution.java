0b673707d583dfc849d88f96dc5a01ab738008fc
krishnasairam <sairamadabala007@msitprogram.net>
2018-09-26 19:49:05 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- ADS-1-assignments/m3/3-sum/Solution.java
+++ ADS-1-assignments/m3/3-sum/Solution.java
@@ -1,29 +1,29 @@
 import java.util.Scanner;
 import java.util.Arrays;
 class Threesum {
-	/**
-	* pairs array used to store inputs.
-	*/
-	int[] pairs;
-	/**
-	 * store number elements added.
-	 */
-	int size;
-	/**
-	 * used to calculate the total pairs.
-	 */
-	Threesum(int n) {
-		size = 0;
-		pairs = new int[n];
-	}
-	public void add(int x) {
-		pairs[size++] = x;
-	}
-	public void threesum() {
-		Arrays.sort(pairs);
-		int j,k;
-		int count = 0;
-		for (int i = 0; i < size - 2; i++) {
+    /**
+    * pairs array used to store inputs.
+    */
+    int[] pairs;
+    /**
+     * store number elements added.
+     */
+    int size;
+    /**
+     * used to calculate the total pairs.
+     */
+    Threesum(int n) {
+        size = 0;
+        pairs = new int[n];
+    }
+    public void add(int x) {
+        pairs[size++] = x;
+    }
+    public void threesum() {
+        Arrays.sort(pairs);
+        int j,k;
+        int count = 0;
+        for (int i = 0; i < size - 2; i++) {
             j = i + 1;
             k = size - 1;
             while (j < k) {
@@ -39,29 +39,31 @@ class Threesum {
             }
         }
         System.out.println(count);
-	}
+    }
 }
-
+/**
+ * Solution class.
+ */
 public final class Solution {
-	/**
-	* Constructs the object.
-	*/
-	private Solution() {
-		// leave this blank
-	}
-	/**
-	 * main function to execute test cases.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
-		Threesum p = new Threesum(n);
-		while (n != 0) {
-			p.add(scan.nextInt());
-			n--;
-		}
-		p.threesum();
-	}
+    /**
+    * Constructs the object.
+    */
+    private Solution() {
+        // leave this blank
+    }
+    /**
+     * main function to execute test cases.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        Threesum p = new Threesum(n);
+        while (n != 0) {
+            p.add(scan.nextInt());
+            n--;
+        }
+        p.threesum();
+    }
 }
