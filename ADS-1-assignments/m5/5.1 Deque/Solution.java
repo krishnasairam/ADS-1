d9133b0d376038fc7b49696c8370d66ac9ad5b09
krishnasairam <sairamadabala007@msitprogram.net>
2018-09-28 16:36:55 +0530

    "check_style_score -> 0 of 5 passed. style score: 1/1 "

---
+++ ADS-1-assignments/m5/5.1 Deque/Solution.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+/**
+ * class solution.
+ */
+public final class Solution {
+    /**
+    * Constructs the object.
+    */
+    private Solution() {
+        // leave this blank
+    }
+    /**
+     * main function to execute test cases.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        Deque d = new Deque();
+        while (n != 0) {
+            String[] tokens = scan.nextLine().split(" ");
+            switch (tokens[0]) {
+            case "pushLeft":
+                d.pushleft(Integer.parseInt(tokens[1]));
+                break;
+            case "pushRight":
+                d.pushright(Integer.parseInt(tokens[1]));
+                break;
+            case "size":
+                d.size();
+                break;
+            case "popLeft":
+                d.popleft();
+                break;
+            case "popright":
+                d.popright();
+                break;
+            default :
+                break;
+            }
+            n--;
+        }
+    }
+}
+
