6b9fd0ad3c9d6c45d7b6c39c3c3c141a2736967f
krishnasairam <sairamadabala007@msitprogram.net>
2018-09-29 10:53:04 +0530

    "check_style_score -> 4 of 12 passed. style score: 0/1 "

---
+++ ADS-1-assignments/m6/Assignment-1/LinkedList.java
@@ -0,0 +1,96 @@
+/*
+ * Java Program to Implement Queue using Linked List
+ */
+import java.util.*;
+/*  Class Node  */
+class Node {
+    protected int data;
+    protected Node link;
+
+    /*  Constructor  */
+    public Node() {
+        link = null;
+        data = 0;
+    }
+    /*  Constructor  */
+    public Node(int d, Node n) {
+        data = d;
+        link = n;
+    }
+    /*  Function to set link to next Node  */
+    public void setLink(Node n) {
+        link = n;
+    }
+    /*  Function to set data to current Node  */
+    public void setData(int d) {
+        data = d;
+    }
+    /*  Function to get link to next node  */
+    public Node getLink() {
+        return link;
+    }
+    /*  Function to get data from current Node  */
+    public int getData() {
+        return data;
+    }
+}
+
+/**
+ * Class for Linkedlist.
+ *
+ * @param      <Item>  The item
+ */
+class LinkedList {
+    protected Node front, rear;
+    public int size;
+    /* Constructor */
+    public LinkedList() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+    /*  Function to check if queue is empty */
+    public boolean isEmpty() {
+        return (front == null && rear == null);
+    }
+    /*  Function to get the size of the queue */
+    public int size() {
+        return size;
+    }
+
+    public void push(int data) {
+        Node nptr = new Node(data, null);
+        size++ ;
+        if (front == null) {
+            front = nptr;
+            rear = front;
+        } else {
+            nptr.setLink(front);
+            front = nptr;
+        }
+    }
+    public int pop() {
+        if (isEmpty() ) {
+        } else {
+            Node ptr = front;
+            front = ptr.getLink();
+            if (front == null) {
+                rear = null;
+            }
+            size-- ;
+            return ptr.getData();
+        } return -1;
+    }
+    public String toString() {
+        String s = "";
+        if (size == 0) {
+            return null;
+        }
+        Node ptr = front;
+        while (ptr != rear.getLink() ) {
+            s += ptr.getData();
+            ptr = ptr.getLink();
+        }
+        return s;
+    }
+}
---
+++ ADS-1-assignments/m6/Assignment-1/Solution.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+/**
+ * Class for add large numbers.
+ */
+class AddLargeNumbers {
+
+    public static LinkedList numberToDigits(String number) {
+        String[] digits = number.split("");
+        LinkedList l = new LinkedList();
+        for (String i : digits) {
+            l.push(Integer.parseInt(i));
+        }
+        return l;
+    }
+
+    public static String digitsToNumber(LinkedList list) {
+        LinkedList l = list;
+        String s = "";
+        while (l.size() != 0) {
+            s += l.pop();
+        } return s;
+    }
+
+    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+        LinkedList l = list1;
+        LinkedList m = list2;
+        LinkedList n = new LinkedList();
+        int carry = 0;
+        int temp = 0;
+        while (l.size() != 0 || m.size() != 0) {
+            if (m.size() == 0) {
+                temp = l.pop();
+            } else if (l.size() == 0) {
+                temp = m.pop();
+            } else {
+                temp = l.pop() + m.pop();
+            }
+            n.push((temp % 10) + carry);
+            carry = temp / 10;
+        }
+        if (carry != 0) {
+            n.push(carry);
+        }
+        return n;
+    }
+}
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String p = sc.nextLine();
+        String q = sc.nextLine();
+        switch (input) {
+        case "numberToDigits":
+            LinkedList pDigits = AddLargeNumbers.numberToDigits(p);
+            LinkedList qDigits = AddLargeNumbers.numberToDigits(q);
+            System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+            System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+            break;
+
+        case "addLargeNumbers":
+            pDigits = AddLargeNumbers.numberToDigits(p);
+            qDigits = AddLargeNumbers.numberToDigits(q);
+            LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+            System.out.println(AddLargeNumbers.digitsToNumber(result));
+            break;
+        }
+    }
+
+}
