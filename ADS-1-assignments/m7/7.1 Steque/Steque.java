03b3164389a4bcc65704c2bef5ec69e5d25c1a68
krishnasairam <sairamadabala007@msitprogram.net>
2018-10-01 10:53:54 +0530

    "check_style_score -> 0 of 2 passed. style score: 1/1 "

---
+++ ADS-1-assignments/m7/7.1 Steque/Steque.java
@@ -0,0 +1,110 @@
+/*
+ * Java Program to Implement Queue using Linked List
+ */
+import java.util.*;
+/*  Class Node  */
+class Node {
+    protected int data;
+    protected Node link;
+
+    /*  Constructor  */
+    public Node() {
+        link = null;
+        data = 0;
+    }
+    /*  Constructor  */
+    public Node(int d, Node n) {
+        data = d;
+        link = n;
+    }
+    /*  Function to set link to next Node  */
+    public void setLink(Node n) {
+        link = n;
+    }
+    /*  Function to set data to current Node  */
+    public void setData(int d) {
+        data = d;
+    }
+    /*  Function to get link to next node  */
+    public Node getLink() {
+        return link;
+    }
+    /*  Function to get data from current Node  */
+    public int getData() {
+        return data;
+    }
+}
+
+/**
+ * Class for deque.
+ *
+ * @param      <Item>  The item
+ */
+class Steque {
+    protected Node front, rear;
+    public int size;
+    /* Constructor */
+    public Steque() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+    /*  Function to check if queue is empty */
+    public boolean isEmpty() {
+        return (front == null && rear == null);
+    }
+    /*  Function to get the size of the queue */
+    public int size() {
+        return size;
+    }
+
+    public void push(int data) {
+        Node nptr = new Node(data, null);
+        size++ ;
+        if (front == null) {
+            front = nptr;
+            rear = front;
+        } else {
+            nptr.setLink(front);
+            front = nptr;
+        }
+    }
+    public void enqueue(int data) {
+        Node nptr = new Node(data, null);
+        size++ ;
+        if (rear == null) {
+            rear = nptr;
+            front = rear;
+        } else {
+            rear.setLink(nptr);
+            rear = nptr;
+        }
+    }
+    public int pop() {
+        if (isEmpty() ) {
+            System.out.println("Steque is empty.");
+        } else {
+            Node ptr = front;
+            front = ptr.getLink();
+            if (front == null) {
+                rear = null;
+            }
+            size-- ;
+            return ptr.getData();
+        } return -1;
+    }
+    public void print() {
+        String s = "";
+        if (size == 0) {
+            //System.out.println("");
+            return ;
+        }
+        Node ptr = front;
+        while (ptr != rear.getLink() ) {
+            s += ptr.getData() + ", ";
+            ptr = ptr.getLink();
+        }
+        s = s.substring(0, s.length() - 2);
+        System.out.println(s);
+    }
+}
