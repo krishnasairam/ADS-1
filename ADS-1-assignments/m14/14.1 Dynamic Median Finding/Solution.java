2ee6382f5764c5ed2fbb759f302f0dc4dffeb53a
krishnasairam <sairamadabala007@msitprogram.net>
2018-10-10 11:58:12 +0530

    "ads1_m14_a1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		//function.
+	}
+	/**
+	 * main.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.nextLine());
+		Heap heap = new Heap();
+		int[] list = new int[n];
+		int i = 0;
+		while (n > 0) {
+			list [i++] = Integer.parseInt(scan.nextLine());
+			if (i > 2) {
+			heap.sort(list, i);
+			float median = ((float)list[(i - 1)/2] + (float)list[(i/2)/2]);
+				System.out.println(median);
+		} else if(i == 2) {
+			System.out.println((list[0] + list[1]) / 2);
+		} else {
+			System.out.println(list[0]);
+		}
+		n--;
+		}
+	}
+}
