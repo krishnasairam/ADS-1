7962d6ddcfa83ec80034c64b52b493a4611bf69f
krishnasairam <sairamadabala007@msitprogram.net>
2018-10-11 18:07:41 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ADS-1-assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -1,37 +1,57 @@
 import java.util.Scanner;
 /**
- * Class for solution.
+ *this class is for main method.
  */
-final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//function.
-	}
-	/**
-	 * main.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int n = Integer.parseInt(scan.nextLine());
-		Heap heap = new Heap();
-		int[] list = new int[n];
-		int i = 0;
-		while (n > 0) {
-			list [i++] = Integer.parseInt(scan.nextLine());
-			if (i > 2) {
-			heap.sort(list, i);
-			float median = (((float)list[(i - 1)/2] + (float)list[(i/2)])/2);
-				System.out.println(median);
-		} else if(i == 2) {
-			System.out.println((float)(list[0] + list[1]) / 2);
-		} else {
-			System.out.println((float)list[0]);
-		}
-		n--;
-		}
-	}
+public class Solution {
+    /**
+     *an empty constructor.
+     */
+    private Solution() {
+    }
+    /**
+     *the main method is to.
+     *take the input from user.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        DynamicMedian obj = new DynamicMedian();
+        long inputs = scan.nextLong();
+        double element = scan.nextDouble();
+        obj.insertAtMin(element);
+        double median = element;
+        System.out.println(median);
+        for (long i = 1; i < inputs; i++) {
+            double input = scan.nextDouble();
+            if (input > median) {
+                obj.insertAtMin(input);
+            } else if (input < median) {
+                obj.insertAtMax(input);
+            } else {
+                obj.insertAtMin(input);
+            }
+            if (obj.getMinSize() - obj.getMaxSize() > 1) {
+                obj.insertAtMax(obj.delMin());
+            }
+            if (obj.getMaxSize() - obj.getMinSize()  > 1) {
+                obj.insertAtMin(obj.delMax());
+            }
+            if (Math.abs(
+                        obj.getMinSize() - obj.getMaxSize()) == 1) {
+                if (obj.getMinSize() > obj.getMaxSize()) {
+                    median = obj.getMin();
+                    System.out.println(median);
+                } else {
+                    median = obj.getMax();
+                    System.out.println(median);
+                }
+            }
+            if (obj.getMinSize()  == obj.getMaxSize()) {
+                double min = obj.getMin();
+                double max = obj.getMax();
+                median = (min + max) / 2.0;
+                System.out.println(median);
+            }
+        }
+    }
 }
