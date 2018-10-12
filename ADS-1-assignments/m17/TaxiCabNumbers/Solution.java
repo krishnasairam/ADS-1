7e8338b95d4815bef406306e8740cdb9dadd6b6c
krishnasairam <sairamadabala007@msitprogram.net>
2018-10-12 19:23:03 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m17/TaxiCabNumbers/Solution.java
+++ ADS-1-assignments/m17/TaxiCabNumbers/Solution.java
@@ -1,27 +1,55 @@
 import java.util.Scanner;
+/**
+ * Class for cube sum.
+ */
 class CubeSum implements Comparable<CubeSum> {
     private final int sum;
     int i;
     int j;
-
-    public CubeSum(int i, int j) {
+/**
+ * Constructs the object.
+ *
+ * @param      i     { parameter_description }
+ * @param      j     { parameter_description }
+ */
+    public CubeSum(final int i, final int j) {
         this.sum = i * i * i + j * j * j;
         this.i = i;
         this.j = j;
     }
-
-    public int compareTo(CubeSum that) {
+/**
+ * { function_description }.
+ *
+ * @param      that  The that
+ *
+ * @return     { description_of_the_return_value }
+ */
+    public int compareTo(final CubeSum that) {
         if (this.sum < that.sum) return -1;
         if (this.sum > that.sum) return +1;
         return 0;
     }
-
+/**
+ * Returns a string representation of the object.
+ *
+ * @return     String representation of the object.
+ */
     public String toString() {
         return sum + " = " + i + "^3" + " + " + j + "^3";
     }
+    /**
+     * get j.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int getj() {
         return j;
     }
+    /**
+     * geti.
+     *
+     * @return     { description_of_the_return_value }
+     */
     public int geti() {
         return i;
     }
