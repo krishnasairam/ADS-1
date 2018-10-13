71bf8acf47a25928c41d7b51bf0072f5ffb65f18
krishnasairam <sairamadabala007@msitprogram.net>
2018-10-13 12:23:39 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1-assignments/m18/Week-3_Assignment-1/BinarySearchTree.java
+++ ADS-1-assignments/m18/Week-3_Assignment-1/BinarySearchTree.java
@@ -142,12 +142,13 @@ class Stock {
     }
     public void print(Stock[] list) {
         for (Stock s : list) {
-            insertAtMax(s);
-            insertAtMin(s);
-        }
-        for (int i = 0; i < 5; i++) {
-            System.out.println(delMin());
-            System.out.println(delMax());
+            System.out.println(s);
+        //     insertAtMax(s);
+        //     insertAtMin(s);
+        // }
+        // for (int i = 0; i < 5; i++) {
+        //     System.out.println(delMin());
+        //     System.out.println(delMax());
         }
     }
 }
