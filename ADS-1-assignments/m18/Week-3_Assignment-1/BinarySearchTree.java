e8fdf1536e9616b64667291a447ebccc6dceb3c1
krishnasairam <sairamadabala007@msitprogram.net>
2018-10-13 12:02:34 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignments/m18/Week-3_Assignment-1/BinarySearchTree.java
@@ -0,0 +1,263 @@
+/**
+ * Class for Stock.
+ */
+class Stock {
+    /**
+     * name.
+     */
+    private String stockname;
+    /**
+     * stockvalue.
+     */
+    private Double stockvalue;
+    /**
+    *the object to access of minheap.
+    */
+    private MinPQ<Stock> min;
+    /**
+     *the object to access of maxheap.
+     */
+    private MaxPQ<Stock> max;
+    /**
+     * frequency.
+     */
+    private int frequency;
+    /**
+     * minarray.
+     */
+    private Stock[] minarray;
+    /**
+     * maxarray;
+     */
+    private Stock[] maxarray;
+    /**
+     * Constructs the object.
+     */
+    Stock() {
+        //emptyconstructor
+    }
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { parameter_description }
+     * @param      a     { parameter_description }
+     * @param      p     { parameter_description }
+     */
+    Stock(final String n, final Double a) {
+        this.stockname = n;
+        this.stockvalue = a;
+        min = new MinPQ<Stock>();
+        max = new MaxPQ<Stock>();
+        frequency = 0;
+        minarray = new Stock[5];
+        maxarray = new Stock[5];
+    }
+    /**
+     * get name.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    String getname() {
+        return this.stockname;
+    }
+    /**
+     * get price.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Double getvalue() {
+        return this.stockvalue;
+    }
+    public int getfrequency() {
+        return this.frequency;
+    }
+    /**
+     *the method will insert the element
+     *to minheap.
+     * @param      item  The item
+     */
+    public void insertAtMin(final Stock item) {
+        min.insert(item);
+    }
+    /**
+     *the method will insert the element
+     *to maxheap.
+     * @param      item  The item
+     */
+    public void insertAtMax(final Stock item) {
+        max.insert(item);
+    }
+    /**
+     *this method gets the minimum element.
+     * from minheap.
+     * @return     The minimum.
+     */
+    public Stock getMin() {
+        return min.min();
+    }
+    /**
+     *this method gets the maximum element.
+     * from maxheap.
+     * @return     The minimum.
+     */
+    public Stock getMax() {
+        return max.max();
+    }
+    /**
+     *this method delete the maximum element.
+     * from maxheap.
+     * @return     The minimum.
+     */
+    public Stock delMax() {
+        return max.delMax();
+    }
+    /**
+     *this method delete the minimum element.
+     * from maxheap.
+     * @return     The minimum.
+     */
+    public Stock delMin() {
+        return min.delMin();
+    }
+    /**
+     * compare.
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int compareTo(final Stock that) {
+        if (this.stockvalue.compareTo(that.stockvalue) > 0) {
+            return 1;
+        } else if (this.stockvalue.compareTo(that.stockvalue) < 0) {
+            return -1;
+        }
+        if (this.stockname.compareTo(that.stockname) > 0) {
+            return 1;
+        } else if (this.stockname.compareTo(that.stockname) < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+    public void print(Stock[] list) {
+        for (Stock s : list) {
+            s.insertAtMax(s);
+            s.insertAtMin(s);
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(delMin());
+            System.out.println(delMax());
+        }
+    }
+}
+/**
+ * Class for binary search tree.
+ */
+public class BinarySearchTree {
+    /**
+     * Class for node.
+     */
+    class Node {
+        /**
+         * key.
+         */
+        private String key;
+        /**
+         * value.
+         */
+        private Integer frequency;
+        /**
+         * left.
+         */
+        private Node left;
+        /**
+         * right.
+         */
+        private Node right;
+        /**
+         * Constructs the object.
+         *
+         * @param      k     { parameter_description }
+         * @param      v     { parameter_description }
+         */
+        Node(final String k, final Integer v) {
+            this.key = k;
+            this.frequency = v;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    /**
+     * root.
+     */
+    private Node root;
+    /**
+     * Constructs the object.
+     */
+    BinarySearchTree() {
+        root = null;
+    }
+    /**
+     * put.
+     *
+     * @param      key    The key
+     * @param      frequency  The frequency
+     */
+    public void put(final String key, final Integer frequency) {
+        root = put(root, key, frequency);
+    }
+    /**
+     * put.
+     *
+     * @param      x      { parameter_description }
+     * @param      key    The key
+     * @param      frequency  The frequency
+     *
+     * @return     { description_of_the_return_frequency }
+     */
+    private Node put(final Node x, final String key, final Integer frequency) {
+        if (x == null) {
+            return new Node(key, frequency);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, frequency);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, frequency);
+        } else {
+            x.frequency = frequency;
+        }
+        return x;
+    }
+    /**
+     * get.
+     *
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_frequency }
+     */
+    public Integer get(final String key) {
+        return get(root, key);
+    }
+    /**
+     * get.
+     *
+     * @param      x     { parameter_description }
+     * @param      key   The key
+     *
+     * @return     { description_of_the_return_frequency }
+     */
+    private Integer get(final Node x, final String key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return get(x.left, key);
+        } else if (cmp > 0) {
+            return get(x.right, key);
+        }
+        return x.frequency;
+    }
+}
---
+++ ADS-1-assignments/m18/Week-3_Assignment-1/Solution.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+/**
+ * solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //function.
+    }
+    /**
+     * main.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BinarySearchTree bst = new BinarySearchTree();
+        int n = sc.nextInt();
+        int m = 6;
+        sc.nextLine();
+        Stock s = new Stock();
+        Stock[] list = new Stock[n];
+        int i = 1;
+        while (m > 0) {
+            while (n > 0) {
+                String[] tokens = sc.nextLine().split(",");
+                list[i++] = new Stock(tokens[0], Double.parseDouble(tokens[1]));
+                n--;
+            }
+            s.print(list);
+            m--;
+        }
+        for (Stock t : list) {
+            bst.put(t.getname(), t.getfrequency());
+        }
+        int p = sc.nextInt();
+        sc.nextLine();
+        while (p > 0) {
+            String[] methods = sc.nextLine().split(",");
+            p--;
+            switch (methods[0]) {
+            case "get":
+                if (methods[1] == "minST") {
+                    break;
+                }
+                if (methods[1] == "maxST") {
+                    break;
+                }
+                break;
+            case "intersection":
+                break;
+            default:
+                break;
+            }
+        }
+    }
+}
