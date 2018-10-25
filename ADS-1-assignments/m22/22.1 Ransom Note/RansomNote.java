b427aff4dc3689fb3877cda4d47e39e7b515c1b5
krishnasairam <sairamadabala007@msitprogram.net>
2018-10-25 19:02:49 +0530

    "ads1_m22_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1-assignments/m22/22.1 Ransom Note/RansomNote.java
@@ -0,0 +1,15 @@
+class RansomNote implements comparable<key, value> {
+	int m, n;
+	String[] a;
+	String[] b;
+	Node key, value;
+	RansomNote(int m, int n, String[] a, String[] b) {
+		this.m = m;
+		this.n = n;
+		this.a = a;
+		this.b = b
+	}
+	class Node {
+
+	}
+}
